8cd22bf1565826240e6063880f20b010c2fbc169
AndrewPertsev <andrewpertsev82@gmail.com>
2021-08-31 22:08:11 +0300

    deleted empty folders

--- epam/andrewpertsev/unit4_class/composition/task01_text/Text.java
+++
@@ -1,7 +0,0 @@
-package epam.andrewpertsev.unit4_class.composition.task01_text;
-
-//1. Создать объект класса Текст, используя классы Предложение, Слово. Методы: дополнить текст, вывести на
-//консоль текст, заголовок текста.
-
-public class Text {
-}
--- epam/andrewpertsev/unit4_class/composition/task03_goverment/Goverment.java
+++
@@ -1,7 +0,0 @@
-package epam.andrewpertsev.unit4_class.composition.task03_goverment;
-
-//3. Создать объект класса Государство, используя классы Область, Район, Город. Методы: вывести на консоль
-//столицу, количество областей, площадь, областные центры.
-
-public class Goverment {
-}
